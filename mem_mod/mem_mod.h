87d5164f897f9390d6f174d06627ad6864869767
DTS0 <dreamoftime0@gmail.com>
2018-11-13 20:53:56 +0800

    makefile中awk的$用$$替换，并增加注释

--- mem_mod/mem_mod.h
+++ mem_mod/mem_mod.h
@@ -6,7 +6,7 @@ struct mem_dev{
 	unsigned long size;
 };
 
-#define MEMDEV_MAJOR 251 //默认主设备号
+//#define MEMDEV_MAJOR 251 //默认主设备号，已弃用，当前为动态获取主设备号
 #define MEMDEV_NR_DEVS 2 //设备数
 #define MEMDEV_SIZE 1024 //大小
 
